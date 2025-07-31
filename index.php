7a82cc0f5600c84aa8c0edca021aeea7acee33ba
maviTrevi1 <141859983+maviTrevi1@users.noreply.github.com>
2025-07-31 15:00:43 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Portafolio Victoria Trevignani</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<!-- Header -->
+			<header id="header">
+				<a href="index.html" class="title">Desarrolladora web: Junior</a>
+				<nav>
+					<ul>
+						<li><a href="index.html"class="active">Mis Proyectos</a></li>
+						<li><a href="generic.html">¿Quién Soy?</a></li>
+						<li><a href="elements.html">General</a></li>
+					</ul>
+				</nav>
+			</header>
+	<body class="is-preload">
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Bienvenido</a></li>
+						    <li><a href="#one">Proyectos</a></li>
+							<!-- <li><a href="#two"></a></li>-->
+							<li><a href="#three">Contacto</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>¡Bienvenido!</h1>
+							<p style="font-size: 10px;">Esta Web personal esta hecha con HTML5 UP</a><br />
+							Bajo la lincencia de <a href="http://html5up.net/license">Creative Commons.</a></p>
+							<ul class="actions">
+								<li><a href="https://html5up.net/" class="button scrolly" style="filter: opacity(60%);">Link</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="#" class="image"><img src="images/pic01.png" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2><a href="https://melodic-snickerdoodle-20a9e4.netlify.app/" class="link"><em>━━ Blog Plantas ━━</em></a></h2>
+									<p>Blog inspirado en plantas de diferentes populares en America del Sur y Argentina.Se podria adaptar a: Un vivero, proyecto ecologico, fotografia, etc.</p>
+									<ul class="actions">
+										<li><a href="https://melodic-snickerdoodle-20a9e4.netlify.app/" class="button">Ver más</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic05.png" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2><a href="https://6851dfa7b10df1f2b2b54ad5--marvelous-meerkat-79f980.netlify.app/" class="link"><em>━━ Tienda Ropa ━━</em></a></h2>
+									<p>Pensada para distintos comercios en este caso una tienda de ropa femenina es erat malesuada quis. Aliquam dapibus.</p>
+									<ul class="actions">
+										<li><a href="https://6851dfa7b10df1f2b2b54ad5--marvelous-meerkat-79f980.netlify.app/" class="button">Ver más</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic08.png" alt="" data-position="25% 25%" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2><a href="https://celadon-kleicha-f3f52f.netlify.app/" class="link"><em>━━ Empresas CocaCola ━━</em></a></h2>
+									<p>Pensado para pequeños empresas que buscan tener su sito web y destacar. Tambien emprendedores que quieren posicionarse en la web.</p>
+									<ul class="actions">
+										<li><a href="https://celadon-kleicha-f3f52f.netlify.app/" class="button">Ver más</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two --> <!--
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>What we do</h2>
+							<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
+							<div class="features">
+								<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>Lorem ipsum amet</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-lock"></span>
+									<h3>Aliquam sed nullam</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-cog"></span>
+									<h3>Sed erat ullam corper</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-desktop"></span>
+									<h3>Veroeros quis lorem</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-link"></span>
+									<h3>Urna quis bibendum</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+								<section>
+									<span class="icon major fa-gem"></span>
+									<h3>Aliquam urna dapibus</h3>
+									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+								</section>
+							</div>
+							<ul class="actions">
+								<li><a href="generic.html" class="button">Learn more</a></li>
+							</ul>
+						</div>
+					</section>-->
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Ponte en contacto conmigo</h2>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="name">Nombre</label>
+												<input type="text" name="name" id="name" />
+											</div>
+											<div class="field half">
+												<label for="email">Mail</label>
+												<input type="text" name="email" id="email" />
+											</div>
+											<div class="field">
+												<label for="message">Mensaje</label>
+												<textarea name="message" id="message" rows="5"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Enviar</a></li>
+										</ul>
+									</form>
+									<?php
+				 	 		if($_SERVER["REQUEST_METHOD"] == "POST")
+				 	 		{
+				 	 			$nombre = $_POST["nombre"] ;
+				 	 			$nombre = $_POST["email"] ;
+				 	 			$nombre = $_POST["mensaje"] ;
+
+				 	 			if(isset($nombre))
+				 	 			{
+				 	 				if(isset($email))
+				 	 				{
+				 	 					if(isset($mensaje))
+				 	 					{
+				 	 						$para = "mavitrevignani1@gmail.com";
+				 	 						$asunto = "Esto es una prueba";
+				 	 						$cuerpo = $nombre."\n".$email."\n".$mensaje;
+				 	 						$adicional = "From: noreply@mavitrevi.com";
+
+				 	 						mail($para,$asunto,$cuerpo,$adicional);
+				 	 						?>
+				 	 							<p>Envio exitoso</p>
+				 	 						<?php
+
+				 	 					}
+				 	 				}
+
+				 	 			}
+
+				 	 		}
+				       ?>
+								</section>
+								
+								<section>
+									
+
+									<ul class="contact">
+										<li>
+											<h3>Direccion</h3>
+											
+											<span>Los Andes s/n<br />
+											Helvecia, Santa Fe<br />
+											AR. Código Postal: 3003</span>
+											
+										</li>
+										<li>
+											<h3>Mail</h3>
+											<a href="https://mail.google.com/mail/u/0/#inbox">mavitrevignani1@gmail</a>
+										</li>
+										<li>
+											<h3>Celular</h3>
+											<span>+549340512383</span>
+										</li>
+										<li>
+											<h3>Social</h3>
+											<ul class="icons">
+												<li><a href="https://www.facebook.com/mavii.tb/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+												<li><a href="https://github.com/maviTrevi1" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="https://www.instagram.com/trevimavi24/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+												<li><a href="https://www.linkedin.com/in/victoria-trevignani-a18060214/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy;Copyright. Todos los derechos reservados.</li><li>Diseño: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+	
+</html>
